bfed5ff4ed668bf1cd772b90c0e0a050fa3ca9e5
Lillian/Ace Thompson <114019309+dumb3mok1d@users.noreply.github.com>
2023-02-28 13:22:09 -0600

    Initial commit

---
+++ Casting_Thompson_Lillian/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Casting_Thompson_Lillian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Ask user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please eneter a whole number!");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console
+            Console.WriteLine("Your favorite number is" + myFavNum);
+
+            // Asks user a true/false question and assigns response to boolean
+            Console.WriteLine("Do you like video games? (Please enter 'true' or 'false') ");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that i like video games!");
+
+
+
+
+          
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Casting_Thompson_Lillian
+ 
